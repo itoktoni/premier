3a864b34318693b5e199a5950cc09b92c2bb6831
itok toni laksono <itok.toni@gmail.com>
2025-05-22 10:26:21 +0700

    update dropdown

--- resources/views/pages/report_rekap_kotor_linen/form.blade.php
+++ resources/views/pages/report_rekap_kotor_linen/form.blade.php
@@ -8,7 +8,9 @@
 
             <input type="hidden" name="report_name" value="Laporan Data Linen">
 
-            <livewire:dropdownrs />
+            <x-form-select col="4" class="search" name="rs_id" label="Rumah Sakit" :options="$rs" />
+            <x-form-select col="4" class="search" name="ruangan_id" label="Ruangan" :options="$ruangan" />
+            <x-form-select col="4" class="search" name="linen_id" label="Jenis Linen" :options="$jenis" />
 
             <x-form-input col="6" type="date" label="Tanggal Awal" name="start_rekap" />
             <x-form-input col="6" type="date" label="Tanggal Akhir" name="end_rekap" />
