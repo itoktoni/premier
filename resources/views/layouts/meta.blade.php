c08f36da650023a9af06fb9a9e8a15412a0bbf0a
itok toni laksono <itok.toni@gmail.com>
2025-08-22 22:38:14 +0700

    update

--- resources/views/layouts/meta.blade.php
+++ resources/views/layouts/meta.blade.php
@@ -3,6 +3,3 @@
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <title>{{ env('APP_NAME') }} - {{ env('APP_DESCRIPTION') }}</title>
 <meta name="csrf-token" content="{{ csrf_token() }}">
-
-<!-- Favicon -->
-{{-- <link rel="shortcut icon" href="{{ url('assets/media/image/favicon.png') }}" /> --}}
